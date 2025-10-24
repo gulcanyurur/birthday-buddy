9df7a8b689869a3cf87ff882b63953cd4836f174
Gülcan Yürür <gulcanyurur92@gmail.com>
2025-10-24 17:20:25 +0300

    feat: added "Add Person" button next to "Clear All"
    - Created a new Add Person button for future form toggle feature
    - Styled buttons to appear side by side using flex layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,33 @@ import data from './data';
 import List from './List';
 
 const App = () => {
-
   const [people, setPeople] = useState(data);
 
   return (
     <main>
       <section className="container">
-        <h3>{people.length}  birthdays today</h3>
-        <List people ={people} />
-        <button type='button'className='btn btn-block' onClick={() => setPeople([])}>clear all</button>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <div className="buttons">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log('Add Person clicked')}
+          >
+            Add Person
+          </button>
+
+          <button
+            type="button"
+            className="btn btn-hipster"
+            onClick={() => setPeople([])}
+          >
+            Clear All
+          </button>
+        </div>
       </section>
     </main>
   );
 };
+
 export default App;
